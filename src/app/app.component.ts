eed6ea698dcd059cef2c4584387c73ff8795435a
JosiMacGyver <joseana.stos.soares@gmail.com>
2024-04-10 19:17:44 -0300

    add locale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  imports:[
+    HomeComponent,
+    RouterModule
+  ],
+  styleUrls: ['./app.component.scss'],
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+
+    <h1 i18n="@@appTitle">My Awesome App</h1>
+
+
+    <h1>Rent Houses!</h1>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
 })
 export class AppComponent {
-  title = 'land-local';
+  title = 'homes';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports:[
+    HomeComponent,
+    RouterModule
+  ],
+  styleUrls: ['./app.component.scss'],
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    <h1>Rent Houses!</h1>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
 })
-export class AppModule { }
+export class AppComponent {
+  title = 'homes';
+}
